e1d127354cabcf671342086e9a8871e149797766
Md. Shamiul Haque Khan <md.shamiulhaque127@gmail.com>
2026-07-19 16:34:42 +0600

    solved #80

---
+++ 80. removeDuplicates2.js
@@ -0,0 +1,22 @@
+function removeDuplicates2(nums) {
+  let left = 0;
+  let right = 0;
+
+  while (right < nums.length) {
+    let count = 1;
+
+    while (right + 1 < nums.length && nums[right] === nums[right + 1]) {
+      right++;
+      count++;
+    }
+
+    for (let i = 0; i < Math.min(2, count); i++) {
+      nums[left] = nums[right];
+      left++;
+    }
+
+    right++;
+  }
+
+  return left;
+}
