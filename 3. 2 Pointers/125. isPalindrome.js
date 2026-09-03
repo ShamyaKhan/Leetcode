705bad5a0b9f53215cb5f4a08f3c6de4d0a9b1c8
Md. Shamiul Haque Khan <md.shamiulhaque127@gmail.com>
2026-09-03 15:01:15 +0600

    solved #125

---
+++ 3. 2 Pointers/125. isPalindrome.js
@@ -0,0 +1,16 @@
+function isPalindrome(s) {
+  const string = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  let left = 0;
+  let right = string.length - 1;
+
+  while (left < right) {
+    if (string[left] !== string[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+}
